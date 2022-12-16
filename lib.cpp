23dba105313d1935111e80fbad5a5374c48d35bd
EdoardoMaestri <115471996+EdoardoMaestri@users.noreply.github.com>
2022-12-16 20:10:17 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int verifica ( char m[10][20] , char nome[20])
+{
+    for(int i=0; i<10; i++)
+    {
+        bool found = true;
+        for(int j=0; j<20; j++)
+        {
+            if(nome[j] != m[i][j])
+            {
+                found = false;
+                break;
+            }
+        }
+        if(found) {
+            cout << i+1;
+            return 0;
+        }
+    }
+
+    return 1;
+}
